bef5908bd9d40e3e9baacc0aff7d47f461eead58
Jay Patel <jpatel8@lion.lmu.edu>
2016-03-29 20:18:27 -0700

    jokes now last commit

--- c-intro/madlib-by-numbers.c
+++ c-intro/madlib-by-numbers.c
@@ -9,19 +9,18 @@ char* madlib_by_numbers(char* template, int word_count, char* words[])
 	{
 		malloclength = strlen(words[i]);
 	}
-	char* sentence = malloc((strlen(template)+malloclength) * word_count);
+	char* sentence = NULL;
+	sentence = (char*)calloc((strlen(template)+malloclength) * word_count+1,sizeof(char));
 	for (int i = 0, j =0; j < strlen(template); ++i,++j)
 	{
 		if(isdigit(template[j]) && template[j]-'0' < word_count)
 		{
 			strcat(sentence, words[template[j]-'0']);
-			printf("%s\n", sentence);
-			i += strlen(words[template[j]-'0'])-1;		}
+			i += strlen(words[template[j]-'0'])-1;
+		}		
 		else
 		{
 			sentence[i] = template[j];
-			printf("%s\n", sentence);
-
 		}
 	}
 	return sentence;
