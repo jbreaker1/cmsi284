98d8135ebc470d8d19f8c9e25e292742e59ec3b3
Jay Patel <jpatel8@lion.lmu.edu>
2016-03-29 19:39:31 -0700

    Adding files

---
+++ c-intro/chord.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+#include <wchar.h>
+
+ int main(int argc, char* argv[]) {
+     char *music[12] = {"A", "A#", "B", "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#"};
+     int position = 0;
+     for(int i = 0; i < 12; i++)
+     	if(strcmp(argv[1],music[i]) == 0)
+     		position = i;
+     printf("Major: %s %s %s\n", music[position],music[(position+4)%12],music[(position+7)%12]);
+     printf("Minor: %s %s %s\n", music[position],music[(position+3)%12],music[(position+7)%12]);
+     printf("7th: %s %s %s %s\n", music[position],music[(position+4)%12],music[(position+7)%12], music[(position+10)%12]);
+     printf("Diminished 7th %s %s %s %s\n", music[position],music[(position+3)%12],music[(position+6)%12],music[(position+9)%12]);
+     return 0;
+ }
