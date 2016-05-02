51351ae5e0f3f11db00f5e00df66c183666396dc
Jay Patel <jpatel8@lion.lmu.edu>
2016-05-02 14:15:08 -0700

    First Homework DOne SICKKKK

---
+++ asm-intro/link.asm
@@ -0,0 +1,10 @@
+	global main
+	section .text
+
+main: 
+	add rsi, 8
+	mov rax, 86
+	mov rdi, [rsi]
+	add rsi, 8
+	mov rsi, [rsi]
+	syscall
---
+++ asm-intro/unlinked.asm
@@ -0,0 +1,8 @@
+	global main
+	section .text
+
+main: 
+	add rsi, 8
+	mov rax, 87
+	mov rdi, [rsi]
+	syscall
