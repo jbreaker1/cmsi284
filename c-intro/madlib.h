9c3fa1b5f8f6bd886854af621df72da89689af8d
Jay Patel <jpatel8@lion.lmu.edu>
2016-05-04 16:50:07 -0700

    YAY MORE .H

--- c-intro/madlib.h
+++ c-intro/madlib.h
@@ -1,8 +1,1 @@
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-char* madlib(char* template, char* adjective, char* noun, char* verb){
-	char *sentence = malloc(strlen(template) + strlen(adjective) + strlen(noun) + strlen(verb));
-	sprintf(sentence, template, adjective, noun, verb);
-	return sentence;
-}
+char* madlib(char* template, char* adjective, char* noun, char* verb);
