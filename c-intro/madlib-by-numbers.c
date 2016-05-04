797999484be0a9ff243a71ade1e57f70737f60b2
Jay Patel <jpatel8@lion.lmu.edu>
2016-05-04 16:44:06 -0700

    I hate committing now

--- c-intro/madlib-by-numbers.c
+++ c-intro/madlib-by-numbers.c
@@ -2,28 +2,22 @@
 #include <string.h>
 #include <stdlib.h>
 #include <ctype.h>
-char* madlib_by_numbers(char* template, int word_count, char* words[])
-{
-	if(word_count == 0)
-	{
-		return template;
+char* madlib_by_numbers(char* template, int word_count, char* words[]) {
+    if (word_count == 0) {
+    	return template;	
+    }
+    int malloclength = 0;
+    for (int i = 0; i < word_count; ++i) {
+    	malloclength = strlen(words[i]);
 	}
-	int malloclength = 0;
-	for (int i = 0; i < word_count; ++i)
-	{
-		malloclength = strlen(words[i]);
-	}
-	char* sentence = NULL;
-	sentence = (char*)calloc((strlen(template)+malloclength) * word_count+1,sizeof(char));
-	for (int i = 0, j =0; j < strlen(template); ++i,++j)
-	{
-		if(isdigit(template[j]) && template[j]-'0' < word_count)
-		{
-			strcat(sentence, words[template[j]-'0']);
-			i += strlen(words[template[j]-'0'])-1;
+    char* sentence = NULL;
+    sentence = (char*)calloc((strlen(template)+malloclength) * word_count+1,sizeof(char));
+	for (int i = 0, j =0; j < strlen(template); ++i,++j) {
+	    if(isdigit(template[j]) && template[j]-'0' < word_count) {
+		    strcat(sentence, words[template[j]-'0']);
+		    i += strlen(words[template[j]-'0'])-1;
 		}		
-		else
-		{
+		else {
 			sentence[i] = template[j];
 		}
 	}
